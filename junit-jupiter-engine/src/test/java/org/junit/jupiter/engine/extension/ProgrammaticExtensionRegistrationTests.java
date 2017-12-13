2bea2ea53bad6ec06b4d9f5c5dbd482c21105591
Sam Brannen <sam@sambrannen.com>
2017-12-13 15:02:27 +0100

    Extract ExtensionUtils and disallow private @RegisterExtension fields
    Issue: #497

--- junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/extension/ProgrammaticExtensionRegistrationTests.java
+++ junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/extension/ProgrammaticExtensionRegistrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2017 the original author or authors.
+ * Copyright 2015-2018 the original author or authors.
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v2.0 which
@@ -119,7 +119,7 @@ class ProgrammaticExtensionRegistrationTests extends AbstractJupiterTestEngineTe
 	static class InstanceLevelExtensionRegistrationWithInjectedExtensionTestCase {
 
 		@RegisterExtension
-		private CrystalBall crystalBall; // Injected by ExtensionInjector.
+		protected CrystalBall crystalBall; // Injected by ExtensionInjector.
 
 		@BeforeEach
 		void beforeEach(String wisdom) {
